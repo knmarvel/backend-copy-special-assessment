2373d71cb8e18f3edaf7730ec0a91dcf2852bfb5
Karen Marvel <knmarvel@gmail.com>
2020-03-03 16:12:35 -0500

    applied formatting fixes and fixed error in copy_to()

--- copyspecial.py
+++ copyspecial.py
@@ -22,6 +22,7 @@ __author__ = "knmarvel with madarp"
 def get_special_paths(dir):
     """takes a directory and returns a list of the absolute
     paths of the special files in the given directory"""
+    
     spec_paths = []
     for root, dirs, files in os.walk(dir):
         for name in files:
@@ -29,13 +30,15 @@ def get_special_paths(dir):
                 if os.path.join(os.path.abspath(dir), name) not in spec_paths:
                     spec_paths.append(os.path.join(os.path.abspath(dir), name))
                 else:
-                    return "ERROR"
+                    print("Error: duplicate special files.")
     return spec_paths
 
 
 def copy_to(paths, dir):
     """given a list of the paths, copy those files into the
     given directory"""
+
+    os.makedirs(dir)
     for file in paths:
         shutil.copy(file, dir)
 
@@ -43,33 +46,39 @@ def copy_to(paths, dir):
 def zip_to(paths, dir):
     """given a list of paths, zip those files up into the
     given zipfile"""
+
     cmd = ["zip", "-j", dir] + paths
+    print("Command I'm going to do " + "\n" + " ".join(cmd))
     subprocess.call(cmd)
 
 
 def parsing():
     """parses arguments given as parameters in calling function"""
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('--fromdir', default='./')
+    parser.add_argument('fromdir', default='./')
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
     return parser.parse_args()
 
 
 def main():
-    """Calls all functions to return above listed results"""
+    """Finds all files with names that include the format '__?___' in the fromdir.
+    If no other arguments are provided, prints out those filenames. If a
+    --todir is provided, copies those files to the todir given. If a
+    --tozip is provided, zips those files to that location."""
+
     args = parsing()
     spec_paths = get_special_paths(args.fromdir)
-    if spec_paths == "ERROR":
-        print("Error: duplicate special files.")
-        return "Error: duplicate special files."
-    print("\n".join(spec_paths))
 
-    if args.todir:
-        copy_to(spec_paths, args.todir)
+    if args.todir or args.tozip:
+        if args.todir:
+            copy_to(spec_paths, args.todir)
 
-    if args.tozip:
-        zip_to(spec_paths, args.tozip)
+        if args.tozip:
+            zip_to(spec_paths, args.tozip)
+    else:
+        print("\n".join(spec_paths))
 
 
 if __name__ == "__main__":
